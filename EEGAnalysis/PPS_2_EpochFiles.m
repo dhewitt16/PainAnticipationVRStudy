6a4f865609a22e4a0fb0ee6e281911a972e7e844
Dr Danielle Hewitt <122755414+dhewitt16@users.noreply.github.com>
2023-12-13 14:53:17 +0000

    Updated

---
+++ EEGAnalysis/PPS_2_EpochFiles.m
@@ -0,0 +1,167 @@
+%% PPS -- Step 2 -- Epoching %
+
+% This loads each subjects' data for each condition, recodes the event
+% markers, does some preprocessing, and saves a merged file of all
+% conditions. Next, a file is prepared for ICA, which allows the ICA to run
+% faster. The ICA weights file is saved, and then added to the original
+% merged file. Data is epoched around left, right and middle cues. After
+% this, data should be opened in EEGLAB for ICA eyeblink removal and
+% artifact rejection.
+
+% Danielle Hewitt (10/08/2023) MOST RECENT VERSION - USE!!!
+
+%% Set paths for EEGLAB
+ eeglab_path = '/Users/dhewitt/Analysis/eeglab2021.0/';
+ addpath(genpath(eeglab_path));
+
+%==========================================================================
+
+%Specify subjects and main directory where data is stored
+cfg = [];
+cfg.dir     = '/Users/dhewitt/Data/pps/';
+%cfg.sub = {'25','26','27','28','29','30'};
+cfg.sub = {'02', '03', '04', '05', '06','08', '09', '10', '11', '12', '13', '14', '15', '16','17','19', '20','21','22','24','25','26','27','28','29','30'};
+cfg.cond   = {'cond-b1', 'cond-b2', 'cond-b3', 'cond-b4'};
+cfg.epoch = [-2.5 5.5];
+
+%==========================================================================
+
+%% Loading the data for all subjects
+
+for iSub = 1:size(cfg.sub,2)
+    currentSubject = cfg.sub{iSub};
+    currentDirectory = [cfg.dir 'P' currentSubject '/'];
+
+    for iCond = 1:4 %loop to run all conditions
+        currentCond = cfg.cond{iCond};
+
+        file2load = dir(fullfile(currentDirectory, char(['*' currentCond '.set'])));
+        wname = [currentDirectory file2load.name];
+
+        if exist(wname) == 0
+            disp(['File ' wname ' does not exist']);
+            return;
+        end
+
+        eeglab redraw;
+        EEG = pop_loadset(wname);
+        EEG = eeg_checkset( EEG );
+
+
+       %% Recoding Event Triggers
+
+        for i = 1:length(EEG.event)
+            if strcmp(EEG.event(i).type, 'S  7')
+                EEG.event(i).type = ['left' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S  8')
+                EEG.event(i).type = ['right' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S  9')
+                EEG.event(i).type = ['middle' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S 10')
+                EEG.event(i).type = ['objAppear' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S 11')
+                EEG.event(i).type = ['objDisappear' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S 17')
+                EEG.event(i).type = ['etFixation' num2str(iCond)]; %eye tracking fixation
+            elseif strcmp(EEG.event(i).type, 'S 12') || strcmp(EEG.event(i).type, 'S 13') %pain-related cues resulting in shocks (75% of trials)
+                EEG.event(i).type = ['PCollShock' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S 18') || strcmp(EEG.event(i).type, 'S 19') %pain-related cues not resulting in shocks due to rand (25% of trials)
+                EEG.event(i).type = ['PCollNoShock' num2str(iCond)];
+            elseif strcmp(EEG.event(i).type, 'S 20') %non-pain-related cues not resulting in shocks
+                EEG.event(i).type = ['nPCollNoShock' num2str(iCond)];
+            end
+        end
+
+
+        %% Now some preprocessing
+        %EEG = pop_reref( EEG, []); %avref
+        EEG = pop_reref( EEG, [10 21] ) %linked mastoids tp9 tp10
+        EEG = eeg_checkset( EEG );
+        EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',70);
+        EEG = eeg_checkset( EEG );
+        EEG = pop_cleanline(EEG, 'bandwidth',2,'chanlist',[1:32] ,'computepower',1,'linefreqs',50,'newversion',0,'normSpectrum',0,'p',0.01,'pad',2,'plotfigures',0,'scanforlines',0,'sigtype','Channels','taperbandwidth',2,'tau',100,'verb',1,'winsize',4,'winstep',1);
+        EEG = eeg_checkset( EEG );
+        EEG=pop_chanedit(EEG, 'load',{'/Users/dhewitt/Analysis/PPS/chanlocs32.sfp','filetype','autodetect'});
+        EEG = eeg_checkset( EEG );
+        %   pop_saveset(EEG,'filename',['P' currentSubject '_' currentCond
+        %   '_preproc.set'],'filepath',currentDirectory);         % uncomment to save the individual cond data file
+
+        eval(['EEG' num2str(iCond) ' = EEG;']);
+
+    end
+
+
+    %==========================================================================
+
+    % Concatenate condition files
+    MEEG = pop_mergeset(EEG1,EEG2,'keepall');
+    MEEG = pop_mergeset(MEEG,EEG3,'keepall');
+    MEEG = pop_mergeset(MEEG,EEG4,'keepall'); MEEG = eeg_checkset( MEEG );
+
+    pop_saveset(MEEG,'filename',['P' currentSubject '_allpreproc.set'],'filepath',currentDirectory); %saving the preproc file for later
+
+    eeglab redraw;
+
+    %% Now running ICA, starting by preparing a streamlined datafile
+    MEEG = pop_eegfiltnew(MEEG, 'locutoff',1,'hicutoff',30); MEEG = eeg_checkset( MEEG ); %filter 1-70 hz
+    %  MEEG = pop_resample(MEEG,100); %this currently isn't working -
+    %  find out why?
+    MEEG = pop_runica(MEEG,'runica');
+
+    pop_topoplot(MEEG, 0, [1:32] ,'Merged datasets',[6 6] ,0,'electrodes','on');
+
+    %saving ICA maps for future reference
+    savename = [cfg.dir 'Figures/P' currentSubject '_ICAMaps.png'];
+    saveas(gcf,savename);
+
+    pop_expica(MEEG, 'weights', [currentDirectory 'P' currentSubject '_combinedICAmatrix']);
+    % pop_saveset(MEEG,'filename',['P' currentSubject_
+    % '_forICA.set'],'filepath',currentDirectory); %saving the setfile
+
+%     % %==========================================================================
+
+
+    %% Next, the epoching around pain-related (left, right) and non-pain-related (middle) cues for each condition (1:4)
+
+    % Looping over all subjects
+
+    for iCond = 1:4 %loop to run all conditions
+        currentCond = cfg.cond{iCond};
+
+        file2load = dir(fullfile(currentDirectory, char(['*' '_allpreproc.set'])));
+        wname = [currentDirectory file2load.name];
+
+        if exist(wname) == 0
+            disp(['File ' wname ' does not exist']);
+            return;
+        end
+
+        [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
+        eeglab redraw;
+        EEG = pop_loadset(wname);
+        EEG = eeg_checkset( EEG );
+
+        %epoching
+         EEG = pop_reref( EEG, [10 21] ) %linked mastoids tp9 tp10
+        EEG = eeg_checkset( EEG );
+        EEG = pop_editset(EEG, 'run', [], 'icaweights', [currentDirectory 'P' currentSubject '_combinedICAmatrix'], 'icasphere', []);
+        EEG = eeg_checkset( EEG );
+        %long epoch
+        % EEG = pop_epoch( EEG, {  'left1' 'left2' 'left3' 'left4' 'right1' 'right2' 'right3' 'right4' 'middle1' 'middle2' 'middle3' 'middle4'}, [-2.5  5.5], 'newname', 'Epoched data', 'epochinfo', 'yes');
+       
+        %short epoch
+        EEG = pop_epoch( EEG, {  'left1' 'left2' 'left3' 'left4' 'right1' 'right2' 'right3' 'right4' 'middle1' 'middle2' 'middle3' 'middle4'}, [-2.5  2.5], 'newname', 'Epoched data', 'epochinfo', 'yes');
+      
+        [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, CURRENTSET, 'setname', 'Continuous EEG Data epochs');
+        eeglab redraw;
+
+        EEG = eeg_checkset( EEG );
+        pop_saveset(EEG,'filename',['P' currentSubject '_allepoched.set'],'filepath',currentDirectory); %saving the file ready for ICA rejection and artifact rejection
+
+    end
+
+end
+
+disp(['Subject T' currentSubject ' epoched, merged file saved after some preprocessing'])
+
+
---
+++ VRAnalysis/getTimestampsVR.m
@@ -0,0 +1,85 @@
+%% This function takes the input as a .json file, with full directory and extension, 
+% and returns a .csv file containing the trial conditions, cues, and
+% timestamps. 
+% Use is getTimestampsVR('/Users/dhewitt/Data/pps/P02/P02___Control_Data__.json','/Users/dhewitt/Data/pps/P02/P02_Extracted',true)
+
+function [condTable] = getTimestampsVR(input, output, dispOption)
+%  GETTIMESTAMPSVR Brief summary of this function.
+%
+% Detailed explanation of this function.
+fileName = input; % filename in JSON extension
+str = fileread(fileName); % dedicated for reading files as text
+data = jsondecode(str); % Using the jsondecode function to parse JSON from string
+
+% Define the target string you want to search for
+targetString = 'start_new_trial_condition';
+
+c=1;
+for i=1:numel(data.o8Y6VNWF7orzDfPGCrJh)
+    structContents = data.o8Y6VNWF7orzDfPGCrJh{i,1};
+    if isfield(structContents, targetString)
+        matchingValues{c,1} = structContents;
+        c=c+1;
+    end
+end
+
+% Create empty cell arrays to store values from the fields
+numTrials = numel(matchingValues);
+start_new_trial_condition = cell(numTrials, 1);
+set_using_cue = cell(numTrials, 1);
+%set_speed_condition = cell(numTrials, 1);
+timestamp = cell(numTrials, 1);
+
+% Extract values from the structs and populate the cell arrays
+for i = 1:numTrials
+    structContents = matchingValues{i};
+
+    % Check if the field exists before accessing it
+    if isfield(structContents, 'start_new_trial_condition')
+        start_new_trial_condition{i} = structContents.start_new_trial_condition;
+    end
+
+    if isfield(structContents, 'set_using_cue')
+        set_using_cue{i} = structContents.set_using_cue;
+    end
+
+    % if isfield(structContents, 'set_speed_condition')
+    %     set_speed_condition{i} = structContents.set_speed_condition;
+    % end
+
+    if isfield(structContents, 'timestamp')
+        timestamp{i} = structContents.timestamp;
+    end
+end
+
+if numTrials == 360
+    condorder = sort([repelem([1,3],108)'; repelem([2,4],72)']);
+    values = [1:108,1:72,1:108,1:72]';
+else
+    disp('Some blocks missing - trial numbers not added - check data log')
+    condorder = zeros(numTrials,2);
+    values = ones(numTrials,2);
+end
+
+% Create a table from the cell arrays
+%T = table(start_new_trial_condition, set_using_cue, set_speed_condition,
+%timestamp);
+T = table(values, condorder, start_new_trial_condition, set_using_cue, timestamp);
+
+% Use the format function to control the display format of the 'timestamp'
+% column
+format long  % Display timestamps in long format
+T.timestamp = cell2mat(T.timestamp);  % Convert cell array to a numeric array
+
+% Display the table
+if dispOption == true
+    disp(T);
+else
+    disp('Table Created')
+end
+
+fileName = [output '.csv'];
+
+writetable(T, fileName);
+
+end
